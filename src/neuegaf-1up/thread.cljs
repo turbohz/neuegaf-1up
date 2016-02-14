231711a3024d85ee4b855bc61adcf408d4c886e5
TurboHz <turbohz@gmail.com>
2016-02-14 18:34:41 +0100

    Implements crude post reversal in thread view

--- src/neuegaf-1up/thread.cljs
+++ src/neuegaf-1up/thread.cljs
@@ -1,8 +1,16 @@
 (ns neuegaf-1up.thread
   (:require [khroma.runtime :as runtime]
             [khroma.log :as console]
-            [cljs.core.async :refer [>! <!]])
+            [cljs.core.async :refer [>! <!]]
+            [dommy.core :refer-macros [sel sel1] :refer [append!]]
+            )
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(defn init []
-      (console/log "This is thread."))
+(defn reverse-posts []
+  (let [posts (reverse (sel "#posts > div"))]
+    (apply append! (sel1 "#posts") posts)
+    ))
+
+  (defn init []
+    (console/log "This is thread.")
+    (reverse-posts))
