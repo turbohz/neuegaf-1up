a1e59961eb8ba0e17cd3890dfb508cc38fa67cf0
TurboHz <turbohz@gmail.com>
2016-06-04 21:22:32 +0200

    Updates dependencies

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
 (defproject neuegaf-1up "0.1.0-SNAPSHOT"
   :license {:name "Unlicense"
             :url "http://unlicense.org"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.228"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.36"]
                  [org.clojure/core.async "0.2.374"]
                  [khroma "0.3.0"]
                  [prismatic/dommy "1.1.0"]]
   :source-paths ["src"]
   :profiles {:dev
              {:plugins [[com.cemerick/austin "0.1.6"]
-                        [lein-cljsbuild "1.1.2"]
+                        [lein-cljsbuild "1.1.3"]
                         [lein-chromebuild "0.3.0"]]
               :cljsbuild
               {:builds
