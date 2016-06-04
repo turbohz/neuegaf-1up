2dad46f3cd71b65c8f23b4c8ddb78d42ce430766
TurboHz <turbohz@gmail.com>
2016-06-04 23:15:11 +0200

    Fixes post reordering
    Mobile site markup is different

--- src/neuegaf-1up/thread.cljs
+++ src/neuegaf-1up/thread.cljs
@@ -2,13 +2,12 @@
   (:require
             [khroma.log :as console]
             [khroma.runtime :as runtime]
-            [dommy.core :refer-macros [sel sel1] :refer [append!]]))
+            [dommy.core :refer-macros [sel sel1] :refer [prepend!]]))
 
 (defn reverse-posts []
-  (let [posts (reverse (sel "#posts > div"))]
-    (apply append! (sel1 "#posts") posts)
-    ))
+  (let [posts (sel ".post")]
+    (apply prepend! (sel1 "#newposts") posts)))
 
-  (defn init []
-    (console/log "This is thread.")
-    (reverse-posts))
+(defn init []
+  (console/log "This is thread.")
+  (reverse-posts))
