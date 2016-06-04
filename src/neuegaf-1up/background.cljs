7ce48718250484878ca8fb4f32ddac123e97f730
TurboHz <turbohz@gmail.com>
2016-06-04 23:00:31 +0200

    Redirects to mobile site

--- src/neuegaf-1up/background.cljs
+++ src/neuegaf-1up/background.cljs
@@ -4,4 +4,27 @@
             [khroma.runtime :as runtime]))
 
 (defn init []
-  (console/log "This is background"))
+  (console/log "This is background")
+
+  (let [
+        request js/chrome.declarativeWebRequest
+        ; matching
+        matcher #js {:url #js {:hostSuffix ".neogaf.com"} :stages #js ["onBeforeRequest" "onBeforeSendHeaders"]}
+        condition (new request.RequestMatcher matcher)
+        ; action1: fake mobile device
+        iphone-agent "Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1"
+        fake-ua (new request.SetRequestHeader #js {:name "User-Agent" :value iphone-agent})
+        ; acion2: move to mobile site
+        redirect-from "^(https?)://www.neogaf.com/forum/(.*)$"
+        redirect-to   "$1://m.neogaf.com/$2"
+        redirect-mobile (new request.RedirectByRegEx #js {:from redirect-from :to redirect-to})
+        rule #js {:conditions #js [condition] :actions #js [fake-ua redirect-mobile]}]
+    (.addRules request.onRequest #js [rule])))
+
+
+
+
+
+
+
+
