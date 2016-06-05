b1ee4fce812187e3f37f50cc05f52fc8a7300f85
TurboHz <turbohz@gmail.com>
2016-06-05 18:05:46 +0200

    Renders forum view on half of the screen

--- src/neuegaf-1up/thread.cljs
+++ src/neuegaf-1up/thread.cljs
@@ -2,12 +2,27 @@
   (:require
             [khroma.log :as console]
             [khroma.runtime :as runtime]
-            [dommy.core :refer-macros [sel sel1] :refer [prepend!]]))
+            [dommy.core :as dommy :refer-macros [sel sel1]]))
 
 (defn reverse-posts []
   (let [posts (sel ".post")]
-    (apply prepend! (sel1 "#newposts") posts)))
+    (apply dommy.core/prepend! (sel1 "#newposts") posts)))
+
+(defn fix-width []
+  (let [
+        body (sel1 "body#forum")
+        children (dommy.utils/->Array (dommy.core/children body))
+        wrapper (dommy/create-element :div)]
+    (dommy/set-attr! wrapper "id" "wrapper")
+    (dommy/append! body wrapper)
+    (console/log children)
+    (apply dommy/append! wrapper (js->clj children))
+    (dommy/set-style! wrapper :width "50%")))
 
 (defn init []
   (console/log "This is thread.")
+  (fix-width)
   (reverse-posts))
+
+
+
